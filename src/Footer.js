464ec8fbc358be09ec494cd6c79619dded022096
mohamed ezzat <mohamad.5halifa1997@gmail.com>
2023-09-21 20:37:55 +0300

    upolde and convert design to react

---
+++ src/Footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Footer(){
+    return(
+        <div>
+            <footer className="text-center p-4">
+                <div className="contianer">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <h5 className="text-light">© Your Website 2022. All Rights Reserved.</h5>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </div>
+    )
+}
+
+export default Footer;
